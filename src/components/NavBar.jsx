2798bf30d05c4ed713320e19f76a24e78c3a5301
AshishChaudhari07 <ashishchaudhari19112003@gmail.com>
2024-10-14 08:06:50 +0530

    name changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ function NavBar() {
     return (
         <div className='flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed'>
             <div>
-                <h1 className='text-5xl font-signature ml-2'>Ashish Chaudhary</h1>
+                <h1 className='text-5xl font-signature ml-2'>Ashish</h1>
             </div>
 
             <ul className='hidden md:flex'>
